801c01e6cfe22cffa546a8b2fca2e95b7c11c5b6
AmanPoddar9 <67048869+AmanPoddar9@users.noreply.github.com>
2024-08-14 13:28:15 +0530

    Update page.jsx

--- client/src/app/sell/page.jsx
+++ client/src/app/sell/page.jsx
@@ -155,7 +155,7 @@ const SellRequestForm = () => {
         <title>Sell Your Car | Best Price & Instant Payment | Real Value</title>
         <meta
           name="description"
-          content="Sell your car quickly and easily with Poddar Motors Real Value. Get the best price, hassle-free process, and instant payment. Submit your car details now!"
+          content="Sell your car quickly and easily with Poddar Motor s Real Value. Get the best price, hassle-free process, and instant payment. Submit your car details now!"
         />
         <meta
           name="keywords"
@@ -192,7 +192,8 @@ const SellRequestForm = () => {
       <div className="text-left md:px-6 bg-custom-black text-custom-seasalt overflow-x-hidden py-16 pt-8">
         <div>
           <div className="text-left max-w-screen-xl mx-auto mb-4 pt-4 md:text-5xl text-3xl font-bold md:px-0 px-4">
-            Sell Quick With Poddar Motors Real Value
+            Sell Quick With Poddar Motors Real Value and get the best value for
+            your car!!
           </div>
           <div className="max-w-screen-xl mx-auto md:px-0 pl-[5vw]">
             <div className="flex flex-col items-center justify-center border-2 border-opacity-30 border-custom-seasalt rounded-md relative  mt-8 w-max">
