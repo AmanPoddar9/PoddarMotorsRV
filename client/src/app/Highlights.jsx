17294d830b9420833f8fb38abdbb62110b510147
AmanPoddar9 <67048869+AmanPoddar9@users.noreply.github.com>
2024-10-20 14:44:11 +0530

    Update Highlights.jsx

--- client/src/app/Highlights.jsx
+++ client/src/app/Highlights.jsx
@@ -52,6 +52,52 @@ const Highlights = () => {
               allowFullScreen
             ></iframe>
           </div>
+          <div className="mb-5">
+            <iframe
+              width="100%"
+              height="250"
+              src="https://www.youtube.com/embed/3kIJQBe0UPs?si=qUDDIZRAdlCb09G1"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>{' '}
+          <div className="mb-5">
+            <iframe
+              width="100%"
+              height="250"
+              src="https://www.youtube.com/embed/bpGkFT4VUko?enablejsapi=1"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>{' '}
+          <div className="mb-5">
+            <iframe
+              width="100%"
+              height="250"
+              src="https://www.youtube.com/embed/bpGkFT4VUko?enablejsapi=1"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/3kIJQBe0UPs?si=qUDDIZRAdlCb09G1"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
