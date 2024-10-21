1a75f75021e20ff59979ec25d4a28683b6dfc210
AmanPoddar9 <67048869+AmanPoddar9@users.noreply.github.com>
2024-10-21 11:51:45 +0530

    Update page.js

--- client/src/app/videos/page.js
+++ client/src/app/videos/page.js
@@ -43,20 +43,22 @@ const Videos = () => {
   ]
 
   return (
-    <div className="p-6">
+    <div className="min-h-screen bg-gray-100 p-6">
       <Head>
         <title>Videos | Poddar Motors Real Value</title>
         <meta
           name="description"
-          content="Watch our latest car videos and reviews."
+          content="Watch our YouTube videos about our used car stock and updates"
         />
         <meta
           name="keywords"
-          content="car videos, used cars in Ranchi, Poddar Motors Real value"
+          content="car videos, used cars in Ranchi, Poddar Motors Real value, second hand bolero, second hand scorpio"
         />
       </Head>
-      <h1 className="text-3xl font-bold mb-4 text-center">Our Car Videos</h1>
-      <p className="mb-8 text-center">
+      <h1 className="text-4xl font-bold mb-6 text-center text-gray-900">
+        YouTube Section
+      </h1>
+      <p className="mb-8 text-center text-gray-700">
         Welcome to our video gallery! Here, you can watch our latest car
         reviews, test drives, and much more. Stay updated with the latest in the
         automotive world and get insights into your favorite cars.
@@ -85,8 +87,8 @@ const Videos = () => {
       </div>
 
       {/* Embed Instagram Reel */}
-      <div className="mt-8">
-        <h2 className="text-xl font-bold mb-4 text-center">
+      <div className="mt-12">
+        <h2 className="text-2xl font-bold mb-4 text-center text-gray-900">
           Featured Instagram Reel
         </h2>
         <blockquote
@@ -109,88 +111,50 @@ const Videos = () => {
               target="_blank"
               rel="noreferrer"
             >
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}
-              >
-                <div
-                  style={{
-                    backgroundColor: '#F4F4F4',
-                    borderRadius: '50%',
-                    flexGrow: 0,
-                    height: '40px',
-                    marginRight: '14px',
-                    width: '40px',
-                  }}
-                ></div>
+              <div className="border border-gray-200 rounded-lg p-4">
                 <div
                   style={{
                     display: 'flex',
-                    flexDirection: 'column',
-                    flexGrow: 1,
-                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    alignItems: 'center',
                   }}
                 >
-                  <div
-                    style={{
-                      backgroundColor: '#F4F4F4',
-                      borderRadius: '4px',
-                      flexGrow: 0,
-                      height: '14px',
-                      marginBottom: '6px',
-                      width: '100px',
-                    }}
-                  ></div>
-                  <div
-                    style={{
-                      backgroundColor: '#F4F4F4',
-                      borderRadius: '4px',
-                      flexGrow: 0,
-                      height: '14px',
-                      width: '60px',
-                    }}
-                  ></div>
+                  <div className="bg-gray-200 rounded-full flex-grow-0 h-10 w-10 mr-2"></div>
+                  <div className="flex flex-col flex-grow-1 justify-center">
+                    <div className="bg-gray-200 rounded h-3 mb-1 w-24"></div>
+                    <div className="bg-gray-200 rounded h-3 w-16"></div>
+                  </div>
                 </div>
-              </div>
-              <div style={{ padding: '19% 0' }}></div>
-              <div
-                style={{
-                  display: 'block',
-                  height: '50px',
-                  margin: '0 auto 12px',
-                  width: '50px',
-                }}
-              >
-                <svg
-                  width="50px"
-                  height="50px"
-                  viewBox="0 0 60 60"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g
-                    stroke="none"
-                    strokeWidth="1"
-                    fill="none"
-                    fillRule="evenodd"
+                <div style={{ padding: '19% 0' }}></div>
+                <div className="flex justify-center items-center mb-3">
+                  <svg
+                    width="50px"
+                    height="50px"
+                    viewBox="0 0 60 60"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
                     <g
-                      transform="translate(-511.000000, -20.000000)"
-                      fill="#000000"
+                      stroke="none"
+                      strokeWidth="1"
+                      fill="none"
+                      fillRule="evenodd"
                     >
-                      <g>
-                        <path d="M556.869,30.41 C554.814,30.41 553.148,32.076 553.148,34.131 C553.148,36.186 554.814,37.852 556.869,37.852 C558.924,37.852 560.59,36.186 560.59,34.131 C560.59,32.076 558.924,30.41 556.869,30.41 M541,60.657 C535.114,60.657 530.342,55.887 530.342,50 C530.342,44.114 535.114,39.342 541,39.342 C546.887,39.342 551.658,44.114 551.658,50 C551.658,55.887 546.887,60.657 541,60.657 M541,33.886 C532.1,33.886 524.886,41.1 524.886,50 C524.886,58.899 532.1,66.113 541,66.113 C549.9,66.113 557.115,58.899 557.115,50 C557.115,41.1 549.9,33.886 541,33.886 M565.378,62.101 C565.244,65.022 564.756,66.606 564.346,67.663 C563.803,69.06 563.154,70.057 562.106,71.106 C561.058,72.155 560.06,72.803 558.662,73.347 C557.607,73.757 556.021,74.244 553.102,74.378 C549.944,74.521 548.997,74.552 541,74.552 C533.003,74.552 532.056,74.521 528.898,74.378 C525.979,74.244 524.393,73.757 523.338,73.347 C521.94,72.803 520.942,72.155 519.894,71.106 C518.846,70.057 518.197,69.06 517.654,67.663 C517.244,66.606 516.755,65.022 516.623,62.101 C516.479,58.943 516.448,57.996 516.448,50 C516.448,42.003 516.479,41.056 516.623,37.899 C516.755,34.978 517.244,33.391 517.654,32.338 C518.197,30.938 518.846,29.942 519.894,28.894 C520.942,27.846 521.94,27.196 523.338,26.654 C524.393,26.244 525.979,25.756 528.898,25.623 C532.057,25.479 533.004,25.448 541,25.448 C548.997,25.448 549.943,25.479 553.102,25.623 C556.021,25.756 557.607,26.244 558.662,26.654 C560.06,27.196 561.058,27.846 562.106,28.894 C563.154,29.942 563.803,30.938 564.346,32.338 C564.756,33.391 565.244,34.978 565.378,37.899 C565.522,41.056 565.552,42.003 565.552,50 C565.552,57.996 565.522,58.943 565.378,62.101 Z" />
+                      <g
+                        transform="translate(-511.000000, -20.000000)"
+                        fill="#000000"
+                      >
+                        <g>
+                          <path d="M556.869,30.41 C554.814,30.41 553.148,32.076 553.148,34.131 C553.148,36.186 554.814,37.852 556.869,37.852 C558.924,37.852 560.59,36.186 560.59,34.131 C560.59,32.076 558.924,30.41 556.869,30.41 M541,60.657 C535.114,60.657 530.342,55.887 530.342,50 C530.342,44.114 535.114,39.342 541,39.342 C546.887,39.342 551.658,44.114 551.658,50 C551.658,55.887 546.887,60.657 541,60.657 M541,33.886 C532.1,33.886 524.886,41.1 524.886,50 C524.886,58.899 532.1,66.113 541,66.113 C549.9,66.113 557.115,58.899 557.115,50 C557.115,41.1 549.9,33.886 541,33.886 M565.378,62.101 C565.244,65.022 564.756,66.606 564.346,67.663 C563.803,69.06 563.154,70.057 562.106,71.106 C561.058,72.155 560.06,72.803 558.662,73.347 C557.607,73.757 556.021,74.244 553.102,74.378 C549.944,74.521 548.997,74.552 541,74.552 C533.003,74.552 532.056,74.521 528.898,74.378 C525.979,74.244 524.393,73.757 523.338,73.347 C521.94,72.803 520.942,72.155 519.894,71.106 C518.846,70.057 518.197,69.06 517.654,67.663 C517.244,66.606 516.755,65.022 516.623,62.101 C516.479,58.943 516.448,57.996 516.448,50 C516.448,42.003 516.479,41.056 516.623,37.899 C516.755,34.978 517.244,33.391 517.654,32.338 C518.197,30.938 518.846,29.942 519.894,28.894 C520.942,27.846 521.94,27.196 523.338,26.654 C524.393,26.244 525.979,25.756 528.898,25.623 C532.057,25.479 533.004,25.448 541,25.448 C548.997,25.448 549.943,25.479 553.102,25.623 C556.021,25.756 557.607,26.244 558.662,26.654 C560.06,27.196 561.058,27.846 562.106,28.894 C563.154,29.942 563.803,30.938 564.346,32.338 C564.756,33.391 565.244,34.978 565.378,37.899 C565.522,41.056 565.552,42.003 565.552,50 C565.552,57.996 565.522,58.943 565.378,62.101 Z" />
+                        </g>
                       </g>
                     </g>
-                  </g>
-                </svg>
+                  </svg>
+                </div>
               </div>
             </a>
           </div>
         </blockquote>
-        <script async src="//www.instagram.com/embed.js"></script>
+        <script async src="//www.instagram.com/embed.js"></script>{' '}
       </div>
     </div>
   )
