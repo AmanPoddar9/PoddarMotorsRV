b6bf2bbaffa0aa0acc3d7c62b952414b32635b5a
Aman Poddar <amanpoddar9@gmail.com>
2025-11-26 11:10:36 +0530

    Fix: Use MONGO_URI from env instead of hardcoded string

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     
-    await mongoose.connect('mongodb+srv://amanpoddar9:poddarmotorsrv@cluster0.9tbfrft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB Connected')
   } catch (error) {
     console.error(error.message)
