aee61dfea57cfe075c45108eb746e12c8cf28701
Aman Poddar <amanpoddar9@gmail.com>
2025-11-26 11:29:58 +0530

    Fix: Update MongoDB URI to correct cluster address

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     
     // Temporarily hardcoding URI to debug Vercel env var issue
-    const uri = 'mongodb+srv://suryaansh2002:suryaansh2002@cluster0.iyrbdvi.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0';
+    const uri = 'mongodb+srv://amanpoddar9:poddarmotorsrv@cluster0.9tbfrft.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(uri)
     console.log('MongoDB Connected')
   } catch (error) {
