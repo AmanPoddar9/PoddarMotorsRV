6597bcf4e3ee0fd2719909b5e77cc77c2721947a
Aman Poddar <amanpoddar9@gmail.com>
2025-11-29 00:07:17 +0530

    fix: correct import path for API_URL in admin testimonial page

--- client/src/app/admin/testimonials/add/page.jsx
+++ client/src/app/admin/testimonials/add/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import axios from 'axios'
-import API_URL from '../../../../config/api'
+import API_URL from '../../../config/api'
 import { useRouter } from 'next/navigation'
 import { FaCloudUploadAlt, FaStar } from 'react-icons/fa'
 
