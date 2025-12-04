468904589b8dfc88dc6d3460e56bacf30bc6b1bb
Aman Poddar <amanpoddar9@gmail.com>
2025-12-04 14:58:44 +0530

    cleanup: Remove redundant /sell-car page and valuation endpoints
    - User prefers using existing /sell page with tiered pricing
    - Removed /sell-car directory
    - Removed valuationController.js and valuationRoutes.js
    - Keeping /sell page with 4 service tiers

--- server/routes/valuationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const valuationController = require('../controllers/valuationController');
-
-// Valuation routes
-router.post('/instant', valuationController.getInstantValuation);
-router.get('/brands', valuationController.getPopularBrands);
-
-module.exports = router;
