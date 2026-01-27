a336809c38c6bf9fa4ac7af1047e325b606a046a
Aman Poddar <amanpoddar9@gmail.com>
2026-01-27 11:50:15 +0530

    fix: Add Toaster to ensure UI feedback is visible

--- client/src/app/providers.jsx
+++ client/src/app/providers.jsx
@@ -2,12 +2,14 @@
 
 import { LanguageProvider } from './contexts/LanguageContext'
 import { CustomerProvider } from './utils/customerContext'
+import { Toaster } from 'react-hot-toast'
 
 export function Providers({ children }) {
   return (
     <LanguageProvider>
       <CustomerProvider>
         {children}
+        <Toaster position="top-right" />
       </CustomerProvider>
     </LanguageProvider>
   )
